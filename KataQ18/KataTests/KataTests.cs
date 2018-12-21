942e53c6af92df9faabb460c1dbea8e1de5721bc
mhhenryss <mhhenryss@gmail.com>
2018-12-21 10:05:58 +0800

    Update Test

--- KataQ18/KataTests/KataTests.cs
+++ KataQ18/KataTests/KataTests.cs
@@ -10,6 +10,9 @@ namespace Kata.Tests
         [TestCase(new int[0], ExpectedResult = 0)]
         [TestCase(new[] { 1, 2, 3, 4 }, ExpectedResult = 10)]
         [TestCase(new[] { 31, -41, 59, 26, -53, 58, 97, -93, -23, 84 }, ExpectedResult = 187)]
+        [TestCase(new[] { 0, 0, 0, 0 }, ExpectedResult = 0)]
+        [TestCase(new[] { 10, -21, 5, 8, -33 }, ExpectedResult = 13)]
+        [TestCase(new[] { 1, 0, 3, 0, 5 }, ExpectedResult = 9)]
         public int TestFromDescription(int[] arr)
         {
             return Kata.LargestSum(arr);
