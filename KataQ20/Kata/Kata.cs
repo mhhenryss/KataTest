1b22bea4fc0f80534fc56da3e53436cb4583409e
mhhenryss <mhhenryss@gmail.com>
2018-12-21 10:38:26 +0800

    Add Q20

---
+++ KataQ20/Kata/Kata.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    
+
+    public class Kata
+    {
+        public static bool IsAnagram(string test, string original)
+        {
+            string testLower = test.ToLower();
+            string originalLower = original.ToLower();
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+
+            foreach (char s in originalLower)
+            {
+                if (dic.ContainsKey(s))
+                {
+                    dic[s] += 1;
+                }
+                else
+                    dic.Add(s, 1);
+            }
+
+            foreach (char s in testLower)
+            {
+                if (dic.ContainsKey(s))
+                {
+                    dic[s] -= 1;
+                    if(dic[s] == 0)
+                        dic.Remove(s);
+                }
+                else
+                    return false;
+            }
+
+            if (dic.Count == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ KataQ20/KataTests/KataTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    class KataTestClass
+    {
+        [Test]
+        [TestCase("foefet", "toffee", ExpectedResult = true)]
+        [TestCase("Buckethead", "DeathCubeK", ExpectedResult = true)]
+        [TestCase("Twoo", "Woot", ExpectedResult = true)]
+        [TestCase("apple", "pale", ExpectedResult = false)]
+        [TestCase("", "", ExpectedResult = true)]
+        [TestCase("a", "", ExpectedResult = false)]
+        [TestCase("", "a", ExpectedResult = false)]
+        public static bool FixedTest(string test, string original)
+        {
+            return Kata.IsAnagram(test, original);
+        }
+
+    }
+}
