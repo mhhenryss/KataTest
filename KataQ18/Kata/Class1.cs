479cd429cae9a54d5a38aee784952acd0f8157b5
mhhenryss <mhhenryss@gmail.com>
2018-12-21 09:27:51 +0800

    Initial commit

---
+++ KataQ18/Kata/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public static class Kata
+    {
+        public static int LargestSum(int[] arr)
+        {
+            // your code to return the largest sum of any subsequence of arr
+            int result = 0;
+            int tempMax = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                tempMax = Math.Max(0, tempMax += arr[i]);
+                result = Math.Max(result, tempMax);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ KataQ18/KataTests/KataTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class KataTest
+    {
+        [TestCase(new[] { -1, -2, -3 }, ExpectedResult = 0)]
+        [TestCase(new int[0], ExpectedResult = 0)]
+        [TestCase(new[] { 1, 2, 3, 4 }, ExpectedResult = 10)]
+        [TestCase(new[] { 31, -41, 59, 26, -53, 58, 97, -93, -23, 84 }, ExpectedResult = 187)]
+        public int TestFromDescription(int[] arr)
+        {
+            return Kata.LargestSum(arr);
+        }
+    }
+}
