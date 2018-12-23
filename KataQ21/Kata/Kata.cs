56ba5d19bbb0aa9c0e56a8c9d337663333a49648
mhhenryss <mhhenryss@gmail.com>
2018-12-23 17:49:23 +0800

    Add Q21

---
+++ KataQ21/Kata/Kata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    
+    public class Kata
+    {
+        public static int[] SortArray(int[] array)
+        {
+            int[] oddarray = array.Where<int>(x => x % 2 == 1).OrderBy(x => x).ToArray<int>();
+            for(int i = 0, j = 0; i < array.Length; i++)
+            {
+                if(array[i] % 2 == 1)
+                {
+                    array[i] = oddarray[j];
+                    j++;
+                }
+            }
+            
+            return array;
+        }
+    }
+}
---
+++ KataQ21/KataTests/KataTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    class KataTestClass
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(new int[] { 1, 3, 2, 8, 5, 4 }, Kata.SortArray(new int[] { 5, 3, 2, 8, 1, 4 }));
+            Assert.AreEqual(new int[] { 1, 3, 5, 8, 0 }, Kata.SortArray(new int[] { 5, 3, 1, 8, 0 }));
+            Assert.AreEqual(new int[] { }, Kata.SortArray(new int[] { }));
+            Assert.AreEqual(new int[] { 0, 0, 0 }, Kata.SortArray(new int[] { 0, 0, 0 }));
+            Assert.AreEqual(new int[] { 1, 3, 5, 7 }, Kata.SortArray(new int[] { 3, 1, 7, 5 }));
+            Assert.AreEqual(new int[] { 2, 4, 8, 6}, Kata.SortArray(new int[] { 2, 4, 8, 6 }));
+        }
+
+    }
+}
