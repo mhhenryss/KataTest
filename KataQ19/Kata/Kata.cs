e510db9abfe7dbfd27bacba172c4e6cb147c472a
mhhenryss <mhhenryss@gmail.com>
2018-12-25 00:19:39 +0800

    Add Q19

---
+++ KataQ19/Kata/Kata.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class Step
+    {
+        private string orientation = "";
+        private int times = 0;
+
+        public Step(string _orientation, int _times)
+        {
+            orientation = _orientation;
+            times = _times;
+        }
+
+        public string Orientation
+        {
+            get { return orientation; }
+        }
+
+        public int Times
+        {
+            get { return times; }
+            set { times = value; }
+        }
+    }
+
+    public class Kata
+    {
+        List<Step> Steps = new List<Step>();
+
+        int stairNow = -1;
+        int stairLast = -1;
+        int firstFloor = -1;
+        public string[] escape(int[,] carpark)
+        {
+            // Code here
+            if (carpark.GetLength(0) == 0)
+                return new string[] { "" };
+
+            GetStart(carpark);
+            stairNow = stairLast;
+
+            for (int floor = firstFloor; floor < carpark.GetLength(0) - 1; floor++)
+            {
+                stairNow = GetStair(carpark, floor);
+                MoveToStair(stairNow, stairLast);
+                MoveDown();
+                stairLast = stairNow;
+            }
+            MoveToEnd(carpark, stairNow);
+
+            return ConverToResult();
+        }
+        
+        private string[] ConverToResult()
+        {
+            string[] result = new string[Steps.Count];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = Steps[i].Orientation + Steps[i].Times;
+            }
+            return result;
+        }
+
+        private void GetStart(int[,] carpark)
+        {
+            for(int i = carpark.GetLength(0) - 1; i >= 0; i--)
+                for(int j = 0; j < carpark.GetLength(1); j++)
+                    if(carpark[i, j] == 2)
+                    {
+                        firstFloor = i;
+                        stairLast = j;
+                        i = -1;
+                        break;
+                    }                       
+        }
+
+        private int GetStair(int[,] carpark, int floor)
+        {
+            for (int i = 0; i < carpark.GetLength(1); i++)
+                if (carpark[floor, i] == 1)
+                    return i;
+            return -1;
+        }
+
+
+        private void MoveToStair(int stairNow, int stairLast)
+        {
+            if (stairLast > stairNow)
+                Steps.Add(new Step("L", (stairLast - stairNow)));
+            else if (stairLast < stairNow)
+                Steps.Add(new Step("R", (stairNow - stairLast)));
+        }
+
+        private void MoveDown()
+        {
+            if (Steps[Steps.Count - 1].Orientation == "D")
+                Steps[Steps.Count - 1].Times += 1;
+            else
+                Steps.Add(new Step("D", 1));
+        }
+
+        private void MoveToEnd(int[,] carpark, int stairNow)
+        {
+            if (carpark.GetLength(1) - 1 > stairNow)
+                Steps.Add(new Step("R", (carpark.GetLength(1) - stairNow - 1)));
+        }
+    }
+}
---
+++ KataQ19/KataTests/KataTests.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    class KataTestClass
+    {
+        [TestCase]
+        public void BasicTest1()
+        {
+            Kata kata = new Kata();
+            int[,] carpark = new int[,] { { 1, 0, 0, 0, 2 },
+                                          { 0, 0, 0, 0, 0 } };
+            string[] result = new string[] { "L4", "D1", "R4" };
+            Assert.AreEqual(result, kata.escape(carpark));
+        }
+
+        [TestCase]
+        public void BasicTest2()
+        {
+            Kata kata = new Kata();
+            int[,] carpark = new int[,] { { 2, 0, 0, 1, 0 },
+                                          { 0, 0, 0, 1, 0 },
+                                          { 0, 0, 0, 0, 0 } };
+            string[] result = new string[] { "R3", "D2", "R1" };
+            Assert.AreEqual(result, kata.escape(carpark));
+        }
+
+        [TestCase]
+        public void BasicTest3()
+        {
+            Kata kata = new Kata();
+            int[,] carpark = new int[,] { { 0, 2, 0, 0, 1 },
+                                          { 0, 0, 0, 0, 1 },
+                                          { 0, 0, 0, 0, 1 },
+                                          { 0, 0, 0, 0, 0 } };
+            string[] result = new string[] { "R3", "D3" };
+            Assert.AreEqual(result, kata.escape(carpark));
+        }
+
+        [TestCase]
+        public void BasicTest4()
+        {
+            Kata kata = new Kata();
+            int[,] carpark = new int[,] { { 1, 0, 0, 0, 2 },
+                                          { 0, 0, 0, 0, 1 },
+                                          { 1, 0, 0, 0, 0 },
+                                          { 0, 0, 0, 0, 0 } };
+            string[] result = new string[] { "L4", "D1", "R4", "D1", "L4", "D1", "R4" };
+            Assert.AreEqual(result, kata.escape(carpark));
+        }
+
+        [TestCase]
+        public void BasicTest5()
+        {
+            Kata kata = new Kata();
+            int[,] carpark = new int[,] { { 0, 0, 0, 0, 2 } };
+            string[] result = new string[] { };
+            Assert.AreEqual(result, kata.escape(carpark));
+        }
+
+        public void BasicTest6()
+        {
+            Kata kata = new Kata();
+            int[,] carpark = new int[,] { { 1, 0, 0, 0, 2 },
+                                          { 0, 0, 0, 0, 1 },
+                                          { 1, 0, 0, 2, 0 },
+                                          { 0, 0, 0, 0, 0 } };
+            string[] result = new string[] { "L3", "R1", "R4" };
+            Assert.AreEqual(result, kata.escape(carpark));
+        }
+
+    }
+}
